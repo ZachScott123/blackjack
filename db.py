d79acbd2ab5cccdfb6191c893839b41ce1ab5e53
Zach Scott <codzapman@gmail.com>
2024-11-29 00:48:06 -0330

    Cleaned up some elements, added try and except to the while loop to catch any errors.

--- db.py
+++ db.py
@@ -6,31 +6,33 @@ def money_choice(money, MONEY): #function to prompt user about money at the star
 
                 while True: #while loop for input
 
-                    create = int(input(f"\n1 - Start with previous balance: [{line}]\n2 - Start with default balance: [100]\n\t  ( 1 / 2 )\n\t      "))
+                    try:
+                        create = int(input(f"\n1 - Start with previous balance: [{line}]\n2 - Start with default balance: [100]\n\t  ( 1 / 2 )\n\t      "))
 
-                    if create == 1:                                   #---Depending on what input given, create or reuse money.txt
-                        print("\nStarting with previous balance.")
-                        money.append(line)
-                        break
-
-                    elif create == 2:
-                        print("\nStarting with default balance.")
-                        with open(MONEY, "w") as file:
-                            file.write("100")
+                        if create == 1:                                   #---Depending on what input given, create or reuse money.txt
+                            print("\nStarting with previous balance.")
                             money.append(line)
                             break
-                        
-                    else:
-                        print("Invalid input.")
+
+                        elif create == 2:
+                            print("\nStarting with default balance.")
+                            with open(MONEY, "w") as file:
+                                file.write("100")
+                                money.append(line)
+                                break
+                            
+                        else:
+                            print("Invalid input.")
+                    except ValueError:
+                        print("Invalid input. Please enter 1 or 2.")
+                        continue
+                    except Exception as e:
+                        print((e), e)
 
     except FileNotFoundError:   #if file not found create new and give user 100 balance
         print(f"Could not find ({MONEY}) file!")
         print("Player starting balance: 100")
         with open(MONEY, "w") as file:
             file.write("100")
-            
-    except ValueError:
-        print("\nInvalid input.")
-
     except Exception as e:
         print((e), e)
